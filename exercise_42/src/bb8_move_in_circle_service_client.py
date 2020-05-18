5c345969f7286a03e4347039afe26faf14d3ee92
wildenali <wildenwildenaliali@gmail.com>
2020-05-18 14:47:26 +0000

    latihan service server dan client

---
+++ exercise_42/src/bb8_move_in_circle_service_client.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+import rospkg
+import rospy
+from std_srvs.srv import Empty, EmptyRequest # you import the service message python classes generated from Empty.srv.
+
+rospy.init_node('service_move_bb8_in_circle_client') # Initialise a ROS node with the name service_client
+rospy.wait_for_service('/move_bb8_in_circle') # Wait for the service client /move_bb8_in_circle to be running
+move_bb8_in_circle_service_client = rospy.ServiceProxy('/move_bb8_in_circle', Empty) # Create the connection to the service
+move_bb8_in_circle_request_object = EmptyRequest() # Create an object of type EmptyRequest
+
+result = move_bb8_in_circle_service_client(move_bb8_in_circle_request_object) # Send through the connection the path to the trajectory file to be executed
+print result # Print the result given by the service called
+
+'''
+Test yg PERTAMA
+Terminal #1
+    roscd exercise_42/launch
+    roslaunch start_bb8_move_in_circle_service_server.launch
+
+Terminal #2
+    roscd exercise_42/launch
+    roslaunch call_bb8_move_in_circle_service_server.launch
+'''
