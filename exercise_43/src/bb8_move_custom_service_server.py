72f11033183e3ffedbd4b9aefb58b859b56e806d
wildenali <wildenwildenaliali@gmail.com>
2020-05-21 02:27:24 +0000

    latihan service server, akses file .srv

---
+++ exercise_43/src/bb8_move_custom_service_server.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+import rospy
+from exercise_43.srv import exercise_43_ServiceMessage, exercise_43_ServiceMessageResponse
+from geometry_msgs.msg import Twist
+
+def my_callback(request):
+    rospy.loginfo("The Service move_bb8_in_circle_custom has been called")
+    move_circle.linear.x = 0.2
+    move_circle.angular.z = 0.2
+    i = 0
+    while i <= request.duration:
+        my_pub.publish(move_circle)
+        rate.sleep()
+        i = i + 1
+    
+    move_circle.linear.x = 0
+    move_circle.angular.z = 0
+    my_pub.publish(move_circle)
+    rospy.loginfo("Finished service move_bb8_in_circle_custom")
+
+    response = exercise_43_ServiceMessageResponse()
+    response.success = True
+    return response     # the service Response class, in this case EmptyResponse
+
+rospy.init_node('service_move_bb8_in_circle_custom_server')
+my_service = rospy.Service('/move_bb8_in_circle_custom', exercise_43_ServiceMessage, my_callback)   # create the Service called move_bb8_in_circle with the defined callback
+my_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+move_circle = Twist()
+rate = rospy.Rate(1)
+rospy.loginfo("Service /move_bb8_in_circle_custom Ready")
+rospy.spin()    # mainain the service open
+
+
+'''
+Tahap PERTAMA
+1. Membuat file .srv di folder srv
+2. Setting configuration di CMakeList.txt
+3. Setting configuration di package.xml
+4. Terminal #1
+    a. cd catkin
+    b. catkin_make
+    c. source devel/setup.bash
+        exercise_43/exercise_43_ServiceMessage  # kalau muncul ini berarti file srv nya udah jadi
+
+Tahap KEDUA
+1. Membuat file bb8_move_custom_service_server.py
+2. Terminal #1
+    a. Eksekusi perintah chmod +x bb8_move_custom_service_server.py di folder exercise_43/src
+    b. roscd; cd .. [ENTER]
+    c. rosrun exercise_43 bb8_move_custom_service_server.py
+
+3. Terminal #2
+    a. rosservice call /move_bb8_in_circle_custom [TAB]+[TAB]
+    b. kemudian ganti durationnya, silahkan masukan angka sesukanya
+    c. and the the BB8 robot berputaaaar, excelent
+'''
